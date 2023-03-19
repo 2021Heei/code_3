460999e73702f21b74caaf16261f9376f3de3c00
wei-mohe <1330359162@qq.com>
2023-03-19 23:43:33 +0800

    二叉搜索树补充

--- BSTree/BSTree.h
+++ BSTree/BSTree.h
@@ -21,6 +21,7 @@ public:
 		:_root(nullptr){}
 	//插入
 	bool insert(K key) {
+		//循环插入
 		if (_root == nullptr) {
 			_root = new Node(key);
 			return true;
@@ -50,9 +51,12 @@ public:
 		}
 		return true;
 	}
+	bool insertR(K key) {
+		return _insertR01(_root, nullptr, key);
+	}
 	//查找
-	bool find(K key) {
-
+	bool find(const K& key) {
+		//循环查找
 		Node* cur = _root;
 		while (cur) {
 			if (key < cur->_key) {
@@ -67,17 +71,18 @@ public:
 		}
 		return false;
 	}
+	bool findR(const K& key) {
+		return _findR(_root, key);
+	}
+	
 	//中序遍历
 	void InOrder() {
 		_InOrder(_root);
 		cout << endl;
 	}
-	Node* findInNode(Node* root, K key) {
-		
-	}
+	
 	//删除
-	void erase(K key) {
-		//
+	bool erase(const K& key) {
 
 		Node* parent = nullptr;
 		Node* cur = _root;
@@ -95,46 +100,62 @@ public:
 				//2.只有左孩子
 				//3.只有右孩子
 				//4.有左右孩子
-				if (cur->_left && cur->_right) {
-					Node* tmp = findInNode(cur->_left);
-					key = tmp->_key;
-					swap(tmp->_key, cur->_key);
-					
-					while (cur) {
-						if (key < cur->_key) {
-							parent = cur;
-							cur = cur->_left;
-						}
-						else if (key > cur->_right) {
-							parent = cur;
-							cur = cur->_right;
+				if (cur->_left == nullptr) {
+					if (parent == nullptr) {
+						_root = cur->_right;
+					}
+					else {
+						if (parent->_left == cur) {
+							parent->_left = cur->_right;
 						}
 						else {
-							//2.只有左孩子
-							if (parent->_left == cur)
-								parent->_left = cur->_left;
-							else
-								parent->_right = cur->_left;
+							parent->_right = cur->_right;
 						}
 					}
 					
+
+					delete cur;
 				}
-				else if (cur->_right) {
-					//3.只有右孩子
-					if (parent->_left == cur)
-						parent->_left = cur->_right;
-					else
-						parent->_right = cur->_right;
+				else if (cur->_right == nullptr) {
+					if (parent == nullptr) {
+						_root = cur->_left;
+					}
+					else {
+						if (parent->_left == cur) {
+							parent->_left = cur->_left;
+						}
+						else {
+							parent->_right = cur->_left;
+						}
+					}
+					delete cur;
 				}
 				else {
-					//2.只有左孩子
-					if (parent->_left == cur)
-						parent->_left = cur->_left;
-					else
-						parent->_right = cur->_left;
+					//左右孩子节点都有
+					//找到cur的右子树的最小节点(最左节点)，替换法删除
+					parent = cur;
+					Node* minRight = cur->_right;
+					while (minRight->_left) {
+						parent = minRight;
+						minRight = minRight->_left;
+					}
+					cur->_key = minRight->_key;
+
+					if (parent->_left == minRight) {
+						parent->_left = minRight->_right;
+					}
+					else {
+						parent->_right = minRight->_right;
+					}
+					delete minRight;
 				}
+				return true;
 			}
 		}
+		return false;
+	}
+	bool eraseR(const K& key) {
+		return _eraseR(_root, key);
 	}
 private:
 	void _InOrder(Node* root) {
@@ -145,16 +166,69 @@ private:
 		cout << root->_key << " ";
 		_InOrder(root->_right);
 	}
+	bool _findR(Node* root, const K& key) {
+		if (root == nullptr) {
+			return false;
+		}
+
+		if (key < root->_key) {
+			return _findR(root->_left, key);
+		}
+		else if(key > root->_key) {
+			return _findR(root->_right, key);
+		}
+		else {
+			return true;
+		}
+	}
+	bool _insertR01(Node* root, Node* parent, const K& key) {
+		if (root == nullptr) {
+			root = new Node(key);
+			if (parent == nullptr) {
+				_root = root;
+			}
+			else {
+				if (key < parent->_key) {
+					parent->_left = root;
+				}
+				else {
+					parent->_right = root;
+				}
+			}
+			
+			return true;
+		}
+		if (key < root->_key) {
+			return _insertR(root->_left, root, key);
+		}
+		else if (key > root->_key) {
+			return _insertR(root->_right, root, key);
+		}
+		else {
+			return false;
+		}
+	}
+	bool _insertR02(Node* root, const K& key) {
+
+	}
+	bool _eraseR(Node* root, const K& key) {
+
+	}
 	Node* _root;
 };
 
 void BSTreeTest01() {
 	BSTree<int> bst;
-	int arr[] = { 3, 2, 1, 5, 4, 8, 7, 10 };
+	int arr[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
 	for (auto e : arr) {
-		bst.insert(e);
+		bst.insertR(e);
 	}
 	bst.InOrder();
-	cout << bst.find(0) << endl;
-	cout << bst.find(3) << endl;
+	cout << bst.findR(0) << endl;
+	cout << bst.findR(3) << endl;
+
+	bst.erase(14);
+	bst.erase(3);
+	bst.erase(8);
+	bst.InOrder();
 }
